d95343178a73a2093d7489a3cbd498fad3daf0f9
Brad Mcallister <bdm@bdmcomputers.com>
2017-10-05 22:03:54 -0500

    Example files
    Added a sample building schema
    Added the template to create the api.ai agent.

--- app.js
+++ app.js
@@ -74,13 +74,13 @@ var msg = {
   instruct: `
   **I\'m the RoomRequest Bot!**  I can help you find and book rooms in your offices!\n\n 
   * To be guided through the process Type: **book a room**, to get started.\n\n
-  * Once you a familiar with the commands you can Type: **book a room at CHG5 tomorrow from 1 to 4**.\n\n 
+  * Once you a familiar with the commands you can Type: **book a room at ORD tomorrow from 1 to 4**.\n\n 
   * You will need to know the building ID you want to book a room at. If you don't know it, [click here](http://www.exmple.com/buildings.html)\n\n 
   * type: **stop** or **cancel** at any time to cancel a request.\n\n 
   * type **support** to join a Spark space to ask questions about this bot\n\n * [click here](https://www.example.com/videodemo.mp4) to watch a quick demo video of roomRequest`,
   cancelled: `You current request has been cancelled.`,
   success: `Great! Your room is now booked and you should receive a calendar invite shortly!`,
-  error: `Sorry, we ran into an issue booking the room!  This is usually caused by an unknown timezone for your region. Please type _support_ to join our spark room. Let us know what building you are trying to book a room at and we can fix it.`,
+  error: `Sorry, we ran into an issue booking the room!  This is usually caused by an unknown timezone for your region or your spark email address doesn't match a mailbox on this exchange system. Please type _support_ to join our spark room. Let us know what building you are trying to book a room at and we can fix it.`,
   support: `You have been added to the Room Request Spark space.`
 }
 
